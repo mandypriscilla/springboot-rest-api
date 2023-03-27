218f22a88a2d91cfcd7b7bf783b2208d49993396
Mandy Priscilla <89837607+mandypriscilla@users.noreply.github.com>
2023-03-27 16:23:13 +0700

    Add files via upload

---
+++ src/main/java/net/javaguides/springbootrestapi/controller/StudentController.java
@@ -0,0 +1,46 @@
+package net.javaguides.springbootrestapi.controller;
+
+import net.javaguides.springbootrestapi.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StudentController {
+
+    // http://localhost:8080/student
+    @GetMapping("student")
+    public Student getStudent(){
+        Student student = new Student(
+                1,
+                "Mandy",
+                "Priscilla"
+        );
+        return student;
+    }
+
+    // http://localhost:8080/students
+    @GetMapping("students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+        students.add(new Student(1, "Mandy", "Priscilla"));
+        students.add(new Student(2, "Ramesh", "Fadatare"));
+        students.add(new Student(3, "Ram", "Jadhav"));
+        students.add(new Student(4, "Sanjay", "Pawar"));
+        return students;
+    };
+
+    // Spring Boot REST API with Path Variable
+    // {id} - URI template variable
+    // http://localhost:8080/students/1
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable(@PathVariable("id") int studentId,
+                                       @PathVariable("first-name") String firstName,
+                                       @PathVariable("last-name") String lastName){
+        return new Student(studentId, firstName, lastName);
+    }
+
+}
